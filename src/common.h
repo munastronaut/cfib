181df8920e736f3bcfa902b768eb9d8462271ac0
munastronaut <96724656+munastronaut@users.noreply.github.com>
2026-02-17 12:54:09 +0800

    Add common header file

---
+++ src/common.h
@@ -0,0 +1,43 @@
+#if defined(__linux__) || defined(__FreeBsd__)
+#define _DEFAULT_SOURCE
+#define _POSIX_C_SOURCE 200809L
+#elif defined(__APPLE__)
+#define _DARWIN_C_SOURCE
+#endif
+
+#include <string.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <gmp.h>
+#include <inttypes.h>
+#include <stdlib.h>
+#if defined(_WIN32)
+#include <windows.h>
+#include <intrin.h>
+#pragma intrinsic(__lzcnt)
+#else
+#include <time.h>
+#endif
+
+#if defined(_WIN32)
+#define COUNT_LEADING_ZEROS(x) __lzcnt64((x))
+#else
+#define COUNT_LEADING_ZEROS(x) __builtin_clzll((x))
+#endif
+
+#define NS_IN_US 1000
+#define NS_IN_MS 1000000
+#define NS_IN_S 1000000000
+
+#define LOG2_PHI 0.694242
+
+#define OUTPUT_NUM (1 << 3)
+#define NO_NEWLINE (1 << 2)
+#define OUTPUT_TIME (1 << 1)
+#define SHOW_HELP (1)
+
+uint64_t get_ns();
+
+void print_calc_time(uint64_t ns);
--- src/lucas.c
+++ src/lucas.c
@@ -1,41 +1,9 @@
-#define _DEFAULT_SOURCE
-#include <string.h>
-#include <stdint.h>
-#include <ctype.h>
-#include <errno.h>
-#include <stdio.h>
-#include <gmp.h>
-#include <inttypes.h>
-#include <stdlib.h>
-#if defined(_WIN32)
-#include <windows.h>
-#include <intrin.h>
-#pragma intrinsic(__lzcnt)
-#elif defined(__APPLE__)
-#include <mach/mach_time.h>
-#else
-#include <time.h>
-#endif
-
-#if defined(_WIN32)
-#define COUNT_LEADING_ZEROS(x) __lzcnt64((x))
-#else
-#define COUNT_LEADING_ZEROS(x) __builtin_clzll((x))
-#endif
-
-#define NS_IN_US 1000
-#define NS_IN_MS 1000000
-#define NS_IN_S 1000000000
-
-#define LOG2_PHI 0.694242
-
-#define OUTPUT_NUM (1 << 3)
-#define NO_NEWLINE (1 << 2)
-#define OUTPUT_TIME (1 << 1)
-#define SHOW_HELP 1
+#include "common.h"
 
 char const *message =
-    "\x1b[4;1mUsage:\x1b[0m \x1b[1m%s\x1b[0m [OPTIONS] [NUMBER]\n\n"
+    "\x1b[4;1mUsage:\x1b[0m\n"
+    "\x1b[1m  %s\x1b[0m [OPTIONS] [NUMBER]\n"
+    "\n"
     "\x1b[4;1mOptions:\x1b[0m\n"
     "\x1b[1m  -n, --number\x1b[0m\t Print number only\n"
     "\x1b[1m  -r, --raw\x1b[0m\t Print number only, without newline\n"
@@ -54,15 +22,6 @@ uint64_t get_ns() {
     QueryPerformanceCounter(&counter);
     return (uint64_t)((counter.QuadPart * INT64_C(1000000000)) /
                       frequency.QuadPart);
-#elif defined(__APPLE__)
-    static mach_timebase_info_data_t info;
-    static int init = 0;
-    if (!init) {
-        mach_timebase_info(&info);
-        init = 1;
-    }
-    uint64_t now = mach_absolute_time();
-    return now * info.numer / info.denom;
 #else
     struct timespec ts;
     clock_gettime(CLOCK_MONOTONIC, &ts);
